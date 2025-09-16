0f91a59a40155627b38d5ed69f59dd84fc1df578
Ivan Bolshakov <is.bolshakov@mail.ru>
2025-09-16 14:55:18 +0300

    build: function for file storage and indicating in db

---
+++ app/utils/file_storage.py
@@ -0,0 +1,26 @@
+import os
+import uuid
+from fastapi import UploadFile
+
+
+async def save_upload_file(upload_file: UploadFile, dest_folder: str) -> str:
+    """
+    Save file to dest_folder.
+    Returns relative path to file (e.g. /media/abc.jpg)
+    """
+
+    os.makedirs(dest_folder, exist_ok=True)
+
+    ext = os.path.splitext(upload_file.filename)[1] if upload_file.filename else ".bin"
+
+
+    file_name = f"{uuid.uuid4().hex}{ext}"
+    file_path = os.path.join(dest_folder, file_name)
+
+    content = await upload_file.read()
+
+    with open(file_path, "wb") as f:
+        f.write(content)
+
+    relative_url = f"/media/{file_name}"
+    return relative_url
